ebd8f84286b7f503d884cc5dab5a324a100c4844
Leo Dion <leogdion@brightdigit.com>
2020-09-26 14:43:49 -0400

    adding italian greetings

--- Sources/hey/Salutation.swift
+++ Sources/hey/Salutation.swift
@@ -1,9 +1,13 @@
 enum Salutation: String {
   case hey
   case hello
+  case ciao
+  case buongiorno
+  case cheers
   case greetings
   case hi
   case howdy
   case welcome
   case bonjour
+  case sup
 }

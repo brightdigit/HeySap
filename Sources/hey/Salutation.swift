73f50f64766c56eac29974439cbde8b8fb5ee986
Leo Dion <leogdion@brightdigit.com>
2020-09-26 14:54:17 -0400

    adding code comments

--- Sources/hey/Salutation.swift
+++ Sources/hey/Salutation.swift
@@ -1,13 +1,53 @@
+/**
+ Different salutations to say Hey! or Hello! with.
+ */
 enum Salutation: String {
+  /**
+   Hey
+   */
   case hey
+  /**
+   Hello
+   */
   case hello
+  /**
+   Ciao
+   */
   case ciao
+  /**
+   Buongiorno
+   */
   case buongiorno
+  /**
+   Cheers
+   */
   case cheers
+  /**
+   Greetings
+   */
   case greetings
+  /**
+   Hi
+   */
   case hi
+  /**
+   Howdy
+   */
   case howdy
+  /**
+   Welcome
+   */
   case welcome
+  /**
+   Bonjour
+   */
   case bonjour
-  case sup
+  /**
+   Sup (with a fist pump 🤜)
+   */
+  case sup = "sup 🤜"
+  /**
+   Heeey (with sunglass emoji 😎)
+   */
+  case heeey = "Heeey 😎"
 }

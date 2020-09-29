ed5e82d003c796abe8fe21da43c7512178709cd0
Leo Dion <leogdion@brightdigit.com>
2020-09-28 21:44:21 -0400

    creating just argument version

--- Sources/hey/Hey.swift
+++ Sources/hey/Hey.swift
@@ -1,22 +1,12 @@
 import ArgumentParser
 
 struct Hey: ParsableCommand {
-  @Flag
-  var verbose = false
-
-  @Option
-  var salutation: Salutation = .hey
-
   @Argument
-  var name: String = "Sap"
+  var name: String // = "Sap"
 
   func run() throws {
     print(
-      "\(salutation.rawValue.capitalizingFirstLetter()) \(name)",
-      terminator: verbose ? "" : "!\n"
+      "Hey \(name)"
     )
-    if verbose {
-      print(", How's Your Day?")
-    }
   }
 }
